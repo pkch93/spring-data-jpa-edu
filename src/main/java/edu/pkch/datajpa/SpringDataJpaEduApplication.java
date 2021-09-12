a17caed8c85cb697c2bd19a7a29da176e3af204f
pkch93 <dev.pkch@gmail.com>
2021-09-12 13:59:05 +0900

    initial commit

---
+++ src/main/java/edu/pkch/datajpa/SpringDataJpaEduApplication.java
@@ -0,0 +1,13 @@
+package edu.pkch.datajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaEduApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataJpaEduApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/pkch/datajpa/SpringDataJpaEduApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.pkch.datajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaEduApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
